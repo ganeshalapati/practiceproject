93d302759784afcb60732edab7c33453ca31fc20
ganeshalapati <82505098+ganeshalapati@users.noreply.github.com>
2021-05-11 22:42:38 +0530

    updated hello java

--- Hello java/src/com/hellojava/java/hellojava.java
+++ Hello java/src/com/hellojava/java/hellojava.java
@@ -3,7 +3,8 @@ package com.hellojava.java;
 public class hellojava {
 	public static void main(String[] args) {
 		System.out.println("hellojava");
-	
+	      	System.out.println("hellogithub");
+
 	}
 
 }
