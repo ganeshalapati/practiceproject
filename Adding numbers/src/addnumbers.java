2cb195d4e9c1944df0a8710d8eeceed0f3764640
ganeshalapati <alapatiganesh31@gmail.com>
2021-05-11 22:01:17 +0530

    added add num project

---
+++ Adding numbers/src/addnumbers.java
@@ -0,0 +1,15 @@
+
+public class addnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a =5, b = 10, c;
+		
+		c = a + b;
+		
+		System.out.println("adding numbers: "+c);
+
+	}
+
+}
